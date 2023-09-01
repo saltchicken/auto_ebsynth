a538dd38346723ed564022139511acd21bda767e
saltchicken <john.eicher89@gmail.com>
2023-09-01 14:44:45 -0700

    Refactored to use pyframes.

--- auto_ebsynth.py
+++ auto_ebsynth.py
@@ -2,39 +2,14 @@ import argparse, subprocess, os, shutil, time
 from image_gridder import grid_joiner, grid_splitter
 from run_a1111 import process_image
 from pyesrgan import run_esrgan
-
-def split_video_to_png(input, output, rate):
-    if os.path.exists(output) and os.path.isdir(output):
-        # TODO Check if files are in the output folder
-        print("Output already exists")
-        # TODO better error checking if output already exists
-        return False
-    else:
-        os.mkdir(output)
-        os.mkdir(output + '/frames')
-        os.mkdir(output + '/keyframes')
-        os.mkdir(output + '/tempkeyframes')
-    
-    ffmpeg_command = [
-        'ffmpeg',
-        '-i', input,
-        '-r', rate,
-        output + '/frames' + '/%05d.png'
-    ]
-
-    # Run the ffmpeg command
-    try:
-        subprocess.run(ffmpeg_command, check=True)
-        print(f'Conversion complete.')
-    except subprocess.CalledProcessError as e:
-        print(f'Error during conversion: {e}')
+from pyframes import video_to_frames
 
 def main():
     parser = argparse.ArgumentParser(description="Automatically setup EBSynth.")
     
     parser.add_argument('-i', '--input', required=True, help='Input video')
     parser.add_argument('-o', '--output', default='output', type=str, help='Output folder')
-    parser.add_argument('-r', '--rate', default='1/1', type=str, help='Frames per second')
+    parser.add_argument('-r', '--rate', default='1', type=str, help='Frames per second')
     # parser.add_argument('--flag', action='store_true', help='Optional flag')
     
     args = parser.parse_args()
@@ -42,8 +17,19 @@ def main():
     # if args.flag:
     #     print('Flag is set')
     output_path = args.output + '_' + args.input.split('.')[0]
+    if os.path.exists(output_path) and os.path.isdir(output_path):
+        # TODO Check if files are in the output folder
+        print("Output already exists")
+        # TODO better error checking if output already exists
+        return False
+    else:
+        os.mkdir(output_path)
+        os.mkdir(output_path + '/frames')
+        os.mkdir(output_path + '/keyframes')
+        os.mkdir(output_path + '/tempkeyframes')
     
-    split_video_to_png(args.input, output_path, args.rate)
+    # split_video_to_png(args.input, output_path, args.rate)
+    video_to_frames(args.input, output_path + '/frames', args.rate)
     image_files = [f for f in os.listdir(output_path + '/frames') if f.endswith('.png')]
     shutil.copy(output_path + '/frames/' + image_files[0], output_path + '/tempkeyframes')
     shutil.copy(output_path + '/frames/' + image_files[int(len(image_files) / 3)], output_path + '/tempkeyframes')
